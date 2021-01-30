d52dfc6defdd05b62fac1bceda1260ec1de78094
angieappiah <alegnalawn2@yahoo.com>
2021-01-30 14:02:32 -0800

    setting up store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux'; //set up store
+import thunk from 'redux-thunk';   //asynchronous
+import {provider} from 'react-redux' //any component wrapped in provider will access to the store
 import App from './App';
 
 ReactDOM.render(<App/>,
