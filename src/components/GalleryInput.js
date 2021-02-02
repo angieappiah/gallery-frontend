c321f71187fd70312049ee17950f32c1bcd24322
angieappiah <alegnalawn2@yahoo.com>
2021-02-01 20:57:18 -0800

    input

--- src/components/GalleryInput.js
+++ src/components/GalleryInput.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+state = {
+    style: ''
+}
+
 class GalleryInput extends React.Component{
     render(){
         return(
             <div>
-                GalleryInput
+                <form>
+                    <label>Create a new style:</label><br></br>
+                    <input type = 'text' placeholder = 'style' value={this.state.style}/>
+                    <input type="submit"/>
+                </form>
             </div>
         )
     }
