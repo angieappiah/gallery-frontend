ba86cc2770f12f05bd4b4011c114470d7530b0d5
angieappiah <alegnalawn2@yahoo.com>
2021-02-02 21:21:14 -0800

    adding events

--- src/components/GalleryInput.js
+++ src/components/GalleryInput.js
@@ -4,13 +4,23 @@ class GalleryInput extends React.Component{
 
     state = {style:'' }
 
+    handleChange = (event) => {
+      this.setState({
+        [event.target.name]: event.target.value
+      })
+    }
+
+    handleSubmit = () =>{
+
+    }
+
 
     render(){
       return(
           <div>
-             <form>
+             <form onSubmit = {this.handleSubmit}>
                  <label> Create A New Style:</label>
-                 <input type= 'text' placeholder= 'Style' value={this.state.style} /><br/>
+                 <input type= 'text' placeholder= 'Style' value={this.state.style} name='style' onChange={this.handleChange} /><br/>
                  <input type= 'submit'/>
             </form> 
           </div>
