9ee5b38f3a928328108458727c2b2d55637fb54e
angieappiah <alegnalawn2@yahoo.com>
2021-02-02 23:12:07 -0800

    adding events

--- src/reducers/galleryReducer.js
+++ src/reducers/galleryReducer.js
@@ -3,6 +3,8 @@ export default function galleryReducer(state = {galleries: []}, action) {
     switch( action.type){
         case 'FETCH_GALLERIES':  
         return {galleries: action.payload}
+        case 'ADD_GALLERY':
+        return {...state, galleries: [...state.galleries, action.payload] }
         default:
             return state
 
