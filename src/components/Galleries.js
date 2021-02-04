e3d7925e32f91dd10746f1d6e958f9423fb5ef5b
angieappiah <alegnalawn2@yahoo.com>
2021-02-04 09:44:03 -0800

    added Links

--- src/components/Galleries.js
+++ src/components/Galleries.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Route, Link } from "react-router-dom";
 import Gallery from './Gallery'
 
 const Galleries = (props) => {
     return(
         <div>
-           {props.galleries.map(gallery=> <div key={gallery.id}><Gallery gallery={gallery}/></div>)} 
+           {props.galleries.map(gallery=>
+           <li key={gallery.id}>
+            <Link to={`/galleries/${gallery.id}`}>{gallery.style}</Link>
+            </li>)} 
         </div>
     )
 }
