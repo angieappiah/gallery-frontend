a2ae49e25f9a7efcb95a48ac37ecf354bf2a1b43
angieappiah <alegnalawn2@yahoo.com>
2021-02-10 04:06:28 -0800

    updated action

--- src/reducers/galleryReducer.js
+++ src/reducers/galleryReducer.js
@@ -1,28 +1,28 @@
 
 export default function galleryReducer(state = {galleries: []}, action) {
-    switch( action.type){
-        case 'FETCH_GALLERIES':  
-        return {galleries: action.payload}
-        case 'ADD_GALLERY':
-        return {...state, galleries: [...state.galleries, action.payload] }
-        case 'ADD_PIECE':
-            let galleries = state.galleries.map(gallery=> {
-                if (gallery.id === action.payload.id) {
-                  return action.payload
-                } else {
-                  return gallery
-                }
-              })
-              return {...state, galleries: galleries}
-            case 'DELETE_PICE':
-                let galleriesTwo = state.galleries.map(gallery => {
-                  if (gallery.id === action.payload.id) {
-                    return action.payload
-                  } else {
-                    return gallery
-                  }
-                  })
-                  return {...state, galleries: galleriesTwo}
+  switch (action.type) {
+    case 'FETCH_GALLERIES':
+      return {galleries: action.payload}
+    case 'ADD_GALLERY':
+      return {...state, galleries: [...state.galleries, action.payload]}
+      case 'ADD_PIECE':
+        let galleries = state.galleries.map(gallery => {
+          if (gallery.id === action.payload.id) {
+            return action.payload
+          } else {
+            return gallery
+          }
+        })
+        return {...state, galleries: galleries}
+        case 'DELETE_PIECE':
+          let galleriesTwo = state.galleries.map(gallery => {
+            if (gallery.id === action.payload.id) {
+              return action.payload
+            } else {
+              return gallery
+            }
+          })
+          return {...state, galleries: galleriesTwo}
         default:
             return state
 
@@ -30,3 +30,6 @@ export default function galleryReducer(state = {galleries: []}, action) {
   
    
 }
+
+        
+
